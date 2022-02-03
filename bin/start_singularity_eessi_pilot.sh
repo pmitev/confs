289123de40d123c817dab80304d3e7b5a854abb8
pmitev <pmitev@gmail.com>
2022-02-03 12:46:21 +0100

    eessi

--- bin/start_singularity_eessi_pilot.sh
+++ bin/start_singularity_eessi_pilot.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+# source of the script
 # https://github.com/EESSI/eessi-demo/blob/main/scripts/start_singularity_eessi_pilot.sh
+
 # honor $TMPDIR if it is already defined, use /tmp otherwise
 if [ -z $TMPDIR ]; then
     export WORKDIR=/tmp/$USER
@@ -12,3 +14,8 @@ export SINGULARITY_BIND="${WORKDIR}/var-run-cvmfs:/var/run/cvmfs,${WORKDIR}/var-
 export SINGULARITY_HOME="${WORKDIR}/home:/home/$USER"
 export EESSI_PILOT="container:cvmfs2 pilot.eessi-hpc.org /cvmfs/pilot.eessi-hpc.org"
 singularity shell --fusemount "$EESSI_PILOT" docker://eessi/client-pilot:centos7-$(uname -m)
+
+# notes
+# https://eessi.github.io/docs/pilot/#accessing-the-eessi-pilot-repository-through-singularity
+# https://www.youtube.com/watch?v=sreSIQHTGL8
+# https://www.youtube.com/watch?v=bZTM4seSI3c&list=PLhnGtSmEGEQgCneeSQvYoIZrbv7wIKlo2
