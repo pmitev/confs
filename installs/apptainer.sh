7996a6831e1d3ccb7bc0405d4edc656afc76463f
pmitev <pmitev@gmail.com>
2024-11-22 11:57:11 +0100

    apptainer.sh

--- installs/apptainer.sh
+++ installs/apptainer.sh
@@ -1,1 +1,1 @@
-cd /tmp && wget https://github.com/apptainer/apptainer/releases/download/v1.3.5/apptainer_1.3.5_amd64.deb && sudo apt install -y ./apptainer_1.3.5_amd64.deb
+cd /tmp && wget https://github.com/apptainer/apptainer/releases/download/v1.3.5/apptainer_1.3.5_amd64.deb && sudo apt update && sudo apt install -y ./apptainer_1.3.5_amd64.deb
