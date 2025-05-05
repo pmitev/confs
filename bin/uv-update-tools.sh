337cf4df297646c3b3942844dd48b6cae24347a8
pmitev <pmitev@gmail.com>
2025-05-05 11:59:30 +0200

    uv

--- bin/uv-update-tools.sh
+++ bin/uv-update-tools.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 uv self update
-uv tool list | awk '$1 != "-"{print $1}' | xargs -n1 uv tool upgrade
+uv tool update --all
+
+#uv tool list | awk '$1 != "-"{print $1}' | xargs -n1 uv tool upgrade
