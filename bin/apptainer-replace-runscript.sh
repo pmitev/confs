645abe5e338e233b8e8049abb7cc0995f7f0958d
pmitev@gmail.com <pmitev@gmail.com>
2025-11-20 08:22:57 +0100

    apptainer-replace-runscript.sh

---
+++ bin/apptainer-replace-runscript.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+url=${1}
+tmpfile=$(mktemp /tmp/container.def.XXXXXX)
+logfile=${tmpfile/def/log}
+line="============================================="
+
+BOOTSTRAP="${url%%://*}"
+FROM="${url#*://}"
+
+DEF="Bootstrap: ${BOOTSTRAP}\n\
+From: ${FROM}\n\
+\n\
+%runscript\n\
+#!/bin/sh\n\
+  if command -v \$SINGULARITY_NAME > /dev/null 2> /dev/null; then\n\
+    exec $SINGULARITY_NAME \"\$@\"\n\
+  else\n\
+    echo \"# ERROR !!! Command $SINGULARITY_NAME not found in the container\"\n\
+  fi"
+
+echo -e ${DEF}
+exit
+
+echo "[I]: container.def in: ${tmpfile}" | tee ${logfile}
+echo "[I]: ${line}" | tee -a ${logfile}
+echo -e ${DEF} | tee ${tmpfile} | tee -a ${logfile}
+echo -e "[I]: ${line}\n" | tee -a ${logfile}
+
+apptainer build container.sif ${tmpfile} |& tee -a ${logfile}
+
+
